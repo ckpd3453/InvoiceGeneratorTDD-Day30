7e59ede16e31ce71a2d1e7d6fb5cdfeea6f5ec46
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-25 21:50:09 +0530

    Addded Problem to Test for total fare of multiple rides

---
+++ src/main/java/Ride.java
@@ -0,0 +1,13 @@
+
+public class Ride {
+	/*
+	 * Data to be used
+	 */
+	public double distance;
+	public int time;
+
+	public Ride(double distance , int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
